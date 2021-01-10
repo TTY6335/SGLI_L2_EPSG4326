803d97e9f9b60e9c8a7fb01a6ab1b3c042a58995
Tetsuya <ogwtetsuya@gmail.com>
2021-01-10 17:44:24 +0900

    とりあえずarrayに読み込めたところまで

--- h5_2_tiff.py
+++ h5_2_tiff.py
@@ -1,9 +1,8 @@
 # coding:utf-8
 import numpy as np
 import gdal, ogr, os, osr,sys
+from osgeo import gdal_array
 import gdalconst
-import h5py
-import math
 
 #タイル番号、画素の位置に対応する緯度経度のメッシュを返す関数
 #4800x4800ピクセルすべての緯度経度を求めても遅い＆gdal_translateでエラーになるので間引き
@@ -65,66 +64,74 @@ if __name__ == '__main__':
 
 
 	try:
-		hdf_file = h5py.File(input_file, 'r')
+#		hdf_file = h5py.File(input_file, 'r')
+		hdf_file = gdal.Open(input_file, gdal.GA_ReadOnly)
 	except:
 		print('%s IS MISSING.' % input_file)
 		exit(1);
 	
-	hdf_file = h5py.File(input_file, 'r')
+	hdf_file = gdal.Open(input_file, gdal.GA_ReadOnly)
+#	print(hdf_file)
+	hdf_arr=gdal.Open(hdf_file.GetSubDatasets()[3][0], gdal.GA_ReadOnly)
+	print(hdf_arr.ReadAsArray())
+#	print(hdf_file.GetMetadata())
+	print(hdf_arr.GetMetadata())
 
 	print('OPEN %s.' % input_file)
+	## Open HDF file
 
+	# Open raster layer
 	#L2のHDF5ファイルのImage_data以下にデータが入っている。
 	try:
 		Image_var=hdf_file['Image_data'][band_name]
 	except:
 		print('%s IS MISSING.' % band_name)
 		print('SELECT FROM')
-		print(hdf_file['Image_data'].keys())
+#		print(hdf_file['Image_data'].keys())
 		exit(1);
 
-	input_file_name=str(hdf_file['Global_attributes'].attrs['Product_file_name'][0][2:45])
-	#L2のHDF5ファイルのImage_data以下にデータが入っている。
-	Image_var=hdf_file['Image_data'][band_name]
-	Slope=hdf_file['Image_data'][band_name].attrs['Slope']
-	Offset=hdf_file['Image_data'][band_name].attrs['Offset']
-	Max_DN=hdf_file['Image_data'][band_name].attrs['Maximum_valid_DN']
-	Min_DN=hdf_file['Image_data'][band_name].attrs['Minimum_valid_DN']
-	
-	#型変換とエラー値をnanに変換する
-	Image_var=np.array(Image_var,dtype='uint16')
-	Image_var=np.where(Image_var>Max_DN,np.nan,Image_var)
-
-	#値を求める
-	Value_arr=Slope*Image_var+Offset
-	Value_arr=np.array(Value_arr,dtype='float32')
-
-	#行数
-	lin_size=Image_var.shape[0]
-	#列数
-	col_size=Image_var.shape[1]
-
-	#GCPのリストをつくる
-	gcp_list=get_geomesh(input_file_name,lin_size,col_size)
-
-	#出力
-	dtype = gdal.GDT_Float32
-	band=1
-	output = gdal.GetDriverByName('GTiff').Create(output_file,lin_size,col_size,band,dtype) 
-	output.GetRasterBand(1).WriteArray(Value_arr)
-	wkt = output.GetProjection()
-	output.SetGCPs(gcp_list,wkt)
-	#与えたGCPを使ってEPSG4326に投影変換
-	output = gdal.Warp(output_file, \
-			output, \
-			dstSRS='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',\
-			srcNodata=np.nan,\
-			dstNodata=np.nan,\
-			tps = True, \
-			outputType=dtype,\
-			multithread=True,\
-			resampleAlg=gdalconst.GRIORA_NearestNeighbour)
-	output.FlushCache()
-	output = None 	
+#	input_file_name=str(hdf_file['Global_attributes'].attrs['Product_file_name'][0][2:45])
+#	#L2のHDF5ファイルのImage_data以下にデータが入っている。
+#	Image_var=hdf_file['Image_data'][band_name]
+#	Slope=hdf_file['Image_data'][band_name].attrs['Slope']
+#	Offset=hdf_file['Image_data'][band_name].attrs['Offset']
+#	Max_DN=hdf_file['Image_data'][band_name].attrs['Maximum_valid_DN']
+#	Min_DN=hdf_file['Image_data'][band_name].attrs['Minimum_valid_DN']
+#	
+#	#型変換とエラー値をnanに変換する
+#	Image_var=np.array(Image_var,dtype='uint16')
+#	Image_var=np.where(Image_var>Max_DN,np.nan,Image_var)
+#
+#	#値を求める
+#	Value_arr=Slope*Image_var+Offset
+#	Value_arr=np.array(Value_arr,dtype='float32')
+#
+#	#行数
+#	lin_size=Image_var.shape[0]
+#	#列数
+#	col_size=Image_var.shape[1]
+#
+#	#GCPのリストをつくる
+#	gcp_list=get_geomesh(input_file_name,lin_size,col_size)
+#
+#	#出力
+#	dtype = gdal.GDT_Float32
+#	band=1
+#	output = gdal.GetDriverByName('GTiff').Create(output_file,lin_size,col_size,band,dtype) 
+#	output.GetRasterBand(1).WriteArray(Value_arr)
+#	wkt = output.GetProjection()
+#	output.SetGCPs(gcp_list,wkt)
+#	#与えたGCPを使ってEPSG4326に投影変換
+#	output = gdal.Warp(output_file, \
+#			output, \
+#			dstSRS='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',\
+#			srcNodata=np.nan,\
+#			dstNodata=np.nan,\
+#			tps = True, \
+#			outputType=dtype,\
+#			multithread=True,\
+#			resampleAlg=gdalconst.GRIORA_NearestNeighbour)
+#	output.FlushCache()
+#	output = None 	
 
 	hdf_file.close()
