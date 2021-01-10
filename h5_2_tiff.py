18189b2e27b257fe062df753381595c5533f7528
Tetsuya <ogwtetsuya@gmail.com>
2021-01-10 23:42:21 +0900

    h5pyを抜きにしてgdalライブラリだけで完結させる

--- h5_2_tiff.py
+++ h5_2_tiff.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 import numpy as np
-import gdal, ogr, os, osr,sys
-from osgeo import gdal_array
+#import gdal, ogr, os, osr,sys
+import gdal,sys
 import gdalconst
 
 #タイル番号、画素の位置に対応する緯度経度のメッシュを返す関数
@@ -11,9 +11,9 @@ def get_geomesh(filename,lintile,coltile):
 
 	#グラニュールIDからタイル番号を取得する
 	#縦方向
-	vtile=int(input_file_name[21:23])
+	vtile=int(filename[21:23])
 	#横方向
-	htile=int(input_file_name[23:25])
+	htile=int(filename[23:25])
    
 	#SGLI/L2であれば固定
 	#縦方向の総タイル数
@@ -62,76 +62,88 @@ if __name__ == '__main__':
 #出力ファイル名
 	output_file=sys.argv[3]
 
-
 	try:
-#		hdf_file = h5py.File(input_file, 'r')
 		hdf_file = gdal.Open(input_file, gdal.GA_ReadOnly)
 	except:
 		print('%s IS MISSING.' % input_file)
 		exit(1);
 	
 	hdf_file = gdal.Open(input_file, gdal.GA_ReadOnly)
-#	print(hdf_file)
-	hdf_arr=gdal.Open(hdf_file.GetSubDatasets()[3][0], gdal.GA_ReadOnly)
-	print(hdf_arr.ReadAsArray())
-#	print(hdf_file.GetMetadata())
-	print(hdf_arr.GetMetadata())
+	dataset_list=hdf_file.GetSubDatasets()
 
 	print('OPEN %s.' % input_file)
 	## Open HDF file
 
 	# Open raster layer
-	#L2のHDF5ファイルのImage_data以下にデータが入っている。
-	try:
-		Image_var=hdf_file['Image_data'][band_name]
-	except:
+	#プロダクト名を探す
+	product_name='//Image_data/'+band_name
+	for dataset_index in range(len(dataset_list)):
+		if(product_name in dataset_list[dataset_index][0]):
+			break;
+
+	if not (product_name in dataset_list[dataset_index][0]):
 		print('%s IS MISSING.' % band_name)
 		print('SELECT FROM')
-#		print(hdf_file['Image_data'].keys())
+		for dataset in dataset_list:
+			if('Image_data' in dataset[0]):
+				print(dataset[0].split('/')[-1])
 		exit(1);
 
-#	input_file_name=str(hdf_file['Global_attributes'].attrs['Product_file_name'][0][2:45])
-#	#L2のHDF5ファイルのImage_data以下にデータが入っている。
-#	Image_var=hdf_file['Image_data'][band_name]
-#	Slope=hdf_file['Image_data'][band_name].attrs['Slope']
-#	Offset=hdf_file['Image_data'][band_name].attrs['Offset']
-#	Max_DN=hdf_file['Image_data'][band_name].attrs['Maximum_valid_DN']
-#	Min_DN=hdf_file['Image_data'][band_name].attrs['Minimum_valid_DN']
-#	
-#	#型変換とエラー値をnanに変換する
-#	Image_var=np.array(Image_var,dtype='uint16')
-#	Image_var=np.where(Image_var>Max_DN,np.nan,Image_var)
-#
-#	#値を求める
-#	Value_arr=Slope*Image_var+Offset
-#	Value_arr=np.array(Value_arr,dtype='float32')
-#
-#	#行数
-#	lin_size=Image_var.shape[0]
-#	#列数
-#	col_size=Image_var.shape[1]
-#
-#	#GCPのリストをつくる
-#	gcp_list=get_geomesh(input_file_name,lin_size,col_size)
-#
-#	#出力
-#	dtype = gdal.GDT_Float32
-#	band=1
-#	output = gdal.GetDriverByName('GTiff').Create(output_file,lin_size,col_size,band,dtype) 
-#	output.GetRasterBand(1).WriteArray(Value_arr)
-#	wkt = output.GetProjection()
-#	output.SetGCPs(gcp_list,wkt)
-#	#与えたGCPを使ってEPSG4326に投影変換
-#	output = gdal.Warp(output_file, \
-#			output, \
-#			dstSRS='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',\
-#			srcNodata=np.nan,\
-#			dstNodata=np.nan,\
-#			tps = True, \
-#			outputType=dtype,\
-#			multithread=True,\
-#			resampleAlg=gdalconst.GRIORA_NearestNeighbour)
-#	output.FlushCache()
-#	output = None 	
+#	print(gdal.Open(hdf_file.GetSubDatasets(),gdal.GA_ReadOnly))
+	Image_var=gdal.Open(hdf_file.GetSubDatasets()[dataset_index][0], gdal.GA_ReadOnly).ReadAsArray()
 
-	hdf_file.close()
+	#Get Sole, Offset,Minimum_valid_DN, Maximum_valid_DN
+	Metadata=hdf_file.GetMetadata_Dict()
+	hdf_filename=Metadata['Global_attributes_Product_file_name']
+
+	for metadata_lavel in Metadata.keys():
+		if band_name+'_Slope' in metadata_lavel:
+			Slope=float(Metadata[metadata_lavel])
+		if band_name+'_Offset' in metadata_lavel:
+			Offset=float(Metadata[metadata_lavel])
+		if band_name+'_Minimum_valid_DN' in metadata_lavel:
+			Minimum_valid_DN=float(Metadata[metadata_lavel])
+		if band_name+'_Maximum_valid_DN' in metadata_lavel:
+			Maximum_valid_DN=float(Metadata[metadata_lavel])
+		if band_name+'_Data_description' in metadata_lavel:
+			Data_description=Metadata[metadata_lavel]
+	#型変換とエラー値をnanに変換する
+	Image_var=np.array(Image_var,dtype='uint16')
+	Image_var=np.where(Image_var>Maximum_valid_DN,np.nan,Image_var)
+
+	#値を求める
+	Value_arr=Slope*Image_var+Offset
+	Value_arr=np.array(Value_arr,dtype='float32')
+
+	#行数
+	lin_size=Image_var.shape[0]
+	#列数
+	col_size=Image_var.shape[1]
+
+	#GCPのリストをつくる
+	gcp_list=get_geomesh(hdf_filename,lin_size,col_size)
+
+	#出力
+	dtype = gdal.GDT_Float32
+	band=1
+	output = gdal.GetDriverByName('GTiff').Create(output_file,lin_size,col_size,band,dtype) 
+	output.GetRasterBand(1).WriteArray(Value_arr)
+	wkt = output.GetProjection()
+	output.SetGCPs(gcp_list,wkt)
+	#与えたGCPを使ってEPSG4326に投影変換
+	output = gdal.Warp(output_file, \
+			output, \
+			dstSRS='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',\
+			srcNodata=np.nan,\
+			dstNodata=np.nan,\
+			tps = True, \
+			outputType=dtype,\
+			multithread=True,\
+			resampleAlg=gdalconst.GRIORA_NearestNeighbour)
+	output.FlushCache()
+	output = None 	
+	print('CREATE'+output_file)
+
+#CLOSE HDF FILE
+	Image_var=None	
+	hdf_file=None
